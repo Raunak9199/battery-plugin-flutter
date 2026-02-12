0cad5ab119dc5043ec35ab87ee338d9f9545f72f
Raunak9199 <raunakraj94543@gmail.com>
2026-02-12 12:35:21 +0530

    Update test imports after package rename

--- test/battery_plugin_test.dart
+++ test/battery_plugin_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:battery_plugin/battery_plugin.dart';
-import 'package:battery_plugin/battery_plugin_platform_interface.dart';
-import 'package:battery_plugin/battery_plugin_method_channel.dart';
+import 'package:nexus_battery_flutter/battery_plugin.dart';
+import 'package:nexus_battery_flutter/battery_plugin_method_channel.dart';
+import 'package:nexus_battery_flutter/battery_plugin_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockBatteryPluginPlatform
